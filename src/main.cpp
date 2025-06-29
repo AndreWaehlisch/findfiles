3b84ab719d555d84831e0301ee2bb901f04e3e4f
André Wählisch <7422075+AndreWaehlisch@users.noreply.github.com>
2025-06-29 22:47:05 +0200

    update

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,14 @@
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication::setOrganizationName("AndreWaehlisch");
-    QCoreApplication::setApplicationName("Find Files");
     const QApplication app(argc, argv);
+    app.setOrganizationName("AndreWaehlisch");
+    app.setApplicationName("Find Files");
+
     const QIcon mainIcon(":/icon.ico");
     Window window;
     window.setWindowIcon(mainIcon);
     window.show();
+
     return app.exec();
 }
